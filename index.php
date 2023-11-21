62f72fcbc5cecd2cda6d86ba475e0859af28ca2c
Kellie Marsh <miss.kellie.marsh@gmail.com>
2023-11-21 12:44:26 +0000

    updated index code to OOP style

--- index.php
+++ index.php
@@ -1,25 +1,17 @@
 <?php
 
+require_once 'src/ProductModel.php';
+
 $db = new PDO('mysql:host=db; dbname=dvdcollection', 'root', 'password');
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->prepare('SELECT `dvd`.`title`, `dvd`.`description`, `dvd`.`run_time`, `dvd`.`image`, `genre`.`genre`, `actors`.`name`
-FROM `dvd`
-	INNER JOIN `genre`
-		ON `dvd`.`genre_id` = `genre`.`id`
-	INNER JOIN `starring`
-		ON `dvd`.`id` = `starring`.`dvd_id`
-	INNER JOIN `actors`
-	  ON `starring`.`actor_id` = `actors`.`id`;');
+$productModel = new ProductModel($db);
 
-
-$query->execute();
-
-$result = $query->fetchAll();
+$product = $productModel->getAllDvds();
 
 echo '<pre>';
-var_dump($result);
+var_dump($product);
 
 
 
