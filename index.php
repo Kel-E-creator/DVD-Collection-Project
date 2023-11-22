cd4a7560cb118622dca3f300629736351224e17a
Kellie Marsh <miss.kellie.marsh@gmail.com>
2023-11-22 13:49:13 +0000

    form html added

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DVD Collection</title>
+</head>
+<body>
+    <h1>DVD Collection</h1>
+    <form method="POST">
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title" />
+
+        <label for="description">Description:</label>
+        <input type="text" id="description" name="description" />
+
+        <label for="run_time">Run Time:</label>
+        <input type="number" id="run_time" name="description" />
+
+        <label for="genre">Genre:</label>
+        <input type="text" id="text" name="text" />
+
+        <label for="starring">Starring:</label>
+        <input type="starring" id="starring" name="starring" />
+
+        <label for="image">Image:</label>
+        <input type="text" id="image" name="image" />
+
+        <input type="submit" value="Add DVD" />
+
+
+    </form>
+    
+</body>
+</html>
+
+
+
+
 <?php
 
 require_once 'src/ProductModel.php';
