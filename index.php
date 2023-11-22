58de1cb1bd7e40f933ed2c58c1975899249a1e82
Kellie Marsh <miss.kellie.marsh@gmail.com>
2023-11-22 14:48:38 +0000

    form updates to include drop down menus

--- index.php
+++ index.php
@@ -18,10 +18,33 @@
         <input type="number" id="run_time" name="description" />
 
         <label for="genre">Genre:</label>
-        <input type="text" id="text" name="text" />
+        <select name="genre" id="genre">
+                <option value="">--- Choose a Genre ---</option>
+                <option value=1>Science Fiction</option>
+                <option value=2>Romantic Comedy</option>
+                <option value=3>Classic 80's Comedy</option>
+                <option value=4>Dystopian</option>
+                <option value=5>Action</option>
+        </select>
+        
+        
 
         <label for="starring">Starring:</label>
+        <select name="starring" id="starring">
+                <option value="">--- Choose an Actor ---</option>
+                <option value=1>Tom Hanks</option>
+                <option value=2>Daryl Hannah</option>
+                <option value=3>John Candy</option>
+                <option value=4>Arnold Schwarzenegger</option>
+                <option value=5>Harrison Ford</option>
+                <option value=6>Sean Young</option>
+                <option value=7>Dolph Lungren</option>
+                <option value=8>Courtney Cox</option>
+
+        </select>
+        <label for="starring">If your actor/actress doesnt appear in the list above please add them here:</label>
         <input type="starring" id="starring" name="starring" />
+
 
         <label for="image">Image:</label>
         <input type="text" id="image" name="image" />
